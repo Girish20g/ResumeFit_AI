415cf75fa98dd7b1dc2f17a31fa12c648d28bf3b
Girish <girishgaba.dev@gmail.com>
2026-05-02 18:00:07 +0530

    fix: added cookie configuration for cors error

--- server/src/controllers/auth.controller.js
+++ server/src/controllers/auth.controller.js
@@ -48,7 +48,12 @@ async function registerUserController(req,res) {
         {expiresIn: "1d"}
     )
 
-    res.cookie("token", token);
+    res.cookie("token", token, {
+        httpOnly: true,
+        secure: true,            
+        sameSite: "none",        
+        maxAge: 24 * 60 * 60 * 1000  // 1 day
+    });
 
     res.status(201).json({
         message: "User registered successfully",
@@ -91,7 +96,12 @@ async function loginUserController(req, res) {
         {expiresIn: "1d"}
     );
 
-    res.cookie("token", token);
+    res.cookie("token", token, {
+        httpOnly: true,
+        secure: true,            
+        sameSite: "none",        
+        maxAge: 24 * 60 * 60 * 1000  // 1 day
+    });
     res.status(200).json({
         message: "User loggedin successfully",
         user: {
